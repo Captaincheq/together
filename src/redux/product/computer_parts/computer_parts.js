3f592e8f586e057f59a7d96b2b9b8ace935fbf4b
Alec Mabhiza Chirawu <alecmabhizachirawu@gmail.com>
2021-02-04 12:27:15 +0800

    u

---
+++ src/redux/product/computer_parts/computer_parts.js
@@ -0,0 +1,37 @@
+const INITIAL_STATE = {
+    sections: [
+      {
+        title: 'Think Pad - 1',
+        imageUrl: 'hats.png',
+        id: 1,
+        linkUrl: 'shop/hats'
+      },
+      {
+        title: 'Think Pad - 2',
+        imageUrl: 'jackets.png',
+        id: 2,
+        linkUrl: 'shop/hats'
+      },
+      {
+        title: 'Think Pad - 3',
+        imageUrl: 'sneakers.png',
+        id: 3,
+        linkUrl: 'shop/hats'
+      },
+      {
+        title: 'Think Pad - 4',
+        imageUrl: 'womens.png',
+        size: 'large',
+        id: 4,
+        linkUrl: 'shop/hats'
+      },
+      {
+        title: 'Think Pad - 5',
+        imageUrl: 'men.png',
+        size: 'large',
+        id: 5,
+        linkUrl: 'shop/hats'
+      }
+  
+    ]
+  };
